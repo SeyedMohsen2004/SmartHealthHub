5b23ba66239e2524a89b99f309aebfed60add0fe
Mohsen <seyed.mohsen83@gmail.com>
2026-06-10 13:37:40 +0330

    fix(ci): resolve flake8 warnings

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,7 +2,7 @@
 
 from decouple import config
 
-from .base import *  # noqa: F403
+from .base import *  # noqa: F401,F403
 
 DEBUG = False
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
--- config/settings/test.py
+++ config/settings/test.py
@@ -1,6 +1,6 @@
 """Test settings."""
 
-from .base import *  # noqa: F403
+from .base import *  # noqa: F401,F403
 
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
